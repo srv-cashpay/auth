6c442f3990f51737868c1f62727f78a8f598ab13
Asep rayana <asep.rayana09@gmail.com>
2025-07-27 14:16:45 +0700

    g sign

--- handlers/auth/google.go
+++ handlers/auth/google.go
@@ -1,8 +1,9 @@
 package handlers
 
 import (
+	"net/http"
+
 	dto "github.com/srv-cashpay/auth/dto/auth"
-	util "github.com/srv-cashpay/util/s"
 	res "github.com/srv-cashpay/util/s/response"
 
 	"github.com/labstack/echo/v4"
@@ -10,21 +11,14 @@ import (
 
 func (h *domainHandler) GoogleSignIn(c echo.Context) error {
 	var req dto.GoogleSignInRequest
-	var resp *dto.AuthResponse
-
-	err := c.Bind(&req)
-	if err != nil {
-		return res.ErrorBuilder(&res.ErrorConstant.BadRequest, err).Send(c)
+	if err := c.Bind(&req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"error": "Invalid request"})
 	}
 
-	resp, err = h.serviceAuth.SignInWithGoogle(req)
+	resp, err := h.serviceAuth.SignInWithGoogle(req)
 	if err != nil {
-		if util.IsDuplicateEntryError(err) {
-			return res.ErrorResponse(&res.ErrorConstant.Duplicate).Send(c)
-		}
-		return res.ErrorResponse(err).Send(c)
+		return c.JSON(http.StatusUnauthorized, map[string]string{"error": err.Error()})
 	}
-
 	return res.SuccessResponse(resp).Send(c)
 
 }
